46edce787d0980b9b184d9a4c2d007698e31b541
muraliravi84 <muralidhara84@gmail.com>
2026-03-28 04:48:35 +0000

    Add fresh Dockerfile, requirements, and app.py at root

---
+++ app.py
@@ -0,0 +1,20 @@
+import joblib
+from huggingface_hub import hf_hub_download
+from flask import Flask, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load model from Hugging Face Hub
+model_path = hf_hub_download(repo_id="Murali0606/tourism-model", filename="tourism_best_model.pkl")
+model = joblib.load(model_path)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    input_data = request.json
+    df = pd.DataFrame([input_data])
+    prediction = model.predict(df)[0]
+    return jsonify({"ProdTaken": int(prediction)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
